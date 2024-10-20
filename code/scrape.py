361d2b9932a88f27fa7409fdd041fff95b30f239
Helene Li <heleneli522@gmail.com>
2024-10-20 18:45:11 -0500

    finish scrape

--- code/scrape.py
+++ code/scrape.py
@@ -8,10 +8,10 @@ from scrape_books import scrape_books
 
 def scrape():
     """Scrape everything and return a list of books."""
-    # Step 1: Scrape all book URLs
+    
     book_urls = scrape_all_pages()
 
-    # Step 2: Scrape all book details from URLs
+    
     books = scrape_books(book_urls)
 
     return books
@@ -19,24 +19,24 @@ def scrape():
 
 def write_books_to_csv(books, path):
     """Writes the list of books to a CSV file."""
-    # Define the CSV header
+    
     fieldnames = ["upc", "title", "category", "description", "price_gbp", "stock"]
 
-    # Write the books data to CSV
+    
     with open(path, mode="w", newline="", encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        # Write the header
+        
         writer.writeheader()
 
-        # Write book data
+        
         for book in books:
             writer.writerow(book)
 
 
 def write_books_to_jsonl(books, path):
     """Writes the list of books to a JSONL file."""
-    # Write the books data to JSONL
+    
     with open(path, mode="w", encoding="utf-8") as jsonlfile:
         for book in books:
             jsonlfile.write(json.dumps(book) + "\n")
@@ -50,9 +50,9 @@ if __name__ == "__main__":
 
     os.makedirs(BASE_DIR, exist_ok=True)
 
-    # Step 3: Scrape all books
+    
     books = scrape()
 
-    # Step 4: Write books to CSV and JSONL
+    
     write_books_to_csv(books, CSV_PATH)
     write_books_to_jsonl(books, JSONL_PATH)
